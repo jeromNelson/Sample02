d614bc437f429336aa0bfcd777480d7a61e55fb5
jeromNelson <jeromnelson35@gmail.com>
2022-05-10 23:14:20 +0530

    built is ready

---
+++ src/org/methods/StringMethods.java
@@ -0,0 +1,31 @@
+package org.methods;
+
+public class StringMethods {
+	
+	public static void main(String[] args) {
+		
+		String s1 = "    java is a programming language";
+		
+		boolean empty = s1.isEmpty();
+		System.out.println(empty);
+		
+		String substring = s1.substring(2);
+		System.out.println(substring);
+		
+		String substring2 = s1.substring(2, 6);
+		System.out.println(substring2);
+		
+		boolean contains = s1.contains(" ");
+		System.out.println(contains);
+		
+		String concat = s1.concat(" and python is alson a proramming language");
+		System.out.println(concat);
+		
+		String trim = s1.trim();
+		System.out.println(trim);
+		
+		String[] l = s1.split("a");
+		System.out.println(l);
+	}
+
+}
